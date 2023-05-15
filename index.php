adbeab19d8547bef9288a25241fb3d498c926ba4
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-05-15 16:45:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-
 require "config/config.php";
 
-if(isset($_POST['submit'])) {
-    // récupération des informations d'identification
+if (isset($_POST['submit'])) {
     $pseudo = $_POST['pseudo'];
     $password = $_POST['password'];
 
-    // vérification des informations d'identification
-     $query = "SELECT users.*, permission.permission FROM users
+    $query = "SELECT users.*, permission.permission FROM users
               JOIN permission ON users.id_permission = permission.id_permission
               WHERE pseudo = ? AND password = ?";
     $stmt = $dbh->prepare($query);
@@ -17,32 +14,30 @@ if(isset($_POST['submit'])) {
     if ($stmt->rowCount() > 0) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $user_role = $row['permission'];
-        
-        // démarrage de la session et stockage des informations d'utilisateur
+        $id_user = $row['id_user']; 
+
         session_start();
         $_SESSION['pseudo'] = $pseudo;
         $_SESSION['permission'] = $user_role;
         $_SESSION['name'] = $name;
         $_SESSION['firstname'] = $firstname;
-        setcookie('pseudo', urlencode($pseudo), time() + 3600, '/');
-       setcookie('pseudo', urlencode($pseudo), time() + 3600, '/');
+        $_SESSION['id_user'] = $id_user;
         setcookie('permission', urlencode($user_role), time() + 3600, '/');
+        setcookie('id_user', urlencode($id_user), time() + 3600, '/');
 
         if ($user_role == 'admin') { 
             header("Location: http://localhost:3000/index.js");
             exit;
+        } else if ($user_role == 'user') {
+            header("Location: http://localhost:3000/index.js");
+            exit;
+        } else {
+            echo "nom d'utilisateur ou mot de passe incorrect";
         }
-      else if ($user_role == 'user'){
-        header("Location: http://localhost:3000/index.js");
-        exit;
-      }
-      else {
+    } else {
         echo "nom d'utilisateur ou mot de passe incorrect";
-      }
     }
-      else {
-        echo "nom d'utilisateur ou mot de passe incorrect";
-      }
+}
 ?>
 
 <html>
@@ -50,7 +45,7 @@ if(isset($_POST['submit'])) {
 
 <body>
 
-<form class = "form" method="post">
+<form class="form" method="post">
 
 Connexion
 
