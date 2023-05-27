0fbe262f8d7548ce7bdb860ba5d8ebf1f3d3be8d
zarcroft <120554160+zarcroft@users.noreply.github.com>
2023-05-27 11:01:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,19 @@ const app = express();
 app.use(cookieParser());
 
 app.get('/user', (req, res) => {
-  // Récupérer la valeur du cookie "permission"
+
   const permission = req.cookies.permission;
 
-  // Récupérer la valeur du cookie "pseudo"
   const id_user = req.cookies.id_user;
 
-  console.log('id_user:', id_user); // Afficher la valeur du cookie "pseudo" dans le terminal
+  console.log('id_user:', id_user); 
 
   if (permission === 'admin') {
-    // Rediriger l'utilisateur vers admin.php
+
     res.redirect('http://localhost/logistic_drone/admin/admin.php');
   } else if (permission === 'user') {
-    // Rediriger l'utilisateur vers user.php
     res.redirect('http://localhost/logistic_drone/user/user.php');
   } else {
-    // Gérer le cas où la permission n'est pas définie ou a une valeur inattendue
     res.send('Erreur : permission invalide');
   }
 });
